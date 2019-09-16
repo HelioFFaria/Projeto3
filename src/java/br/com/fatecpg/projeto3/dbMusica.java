eb4168f49409b5a7ffc7e0c2d3645af5e75a5888
Tales Caio Pariotto <tales@parisotto.com.br>
2019-09-15 22:24:22 -0300

    Update dbMusica.java

--- src/java/br/com/fatecpg/projeto3/dbMusica.java
+++ src/java/br/com/fatecpg/projeto3/dbMusica.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.fatecpg.projeto3;
 
-/**
- *
- * @author helio
- */
+import java.util.ArrayList;
+
 public class dbMusica {
-    
+   private static ArrayList<musica> musica;   
+   
+    public static ArrayList<musica> getMusica(){
+         if ( musica == null){
+            musica = new ArrayList<> ();
+            musica.add(new musica("Closer","Pop","2019"));
+            musica.add(new musica("Tremor","Eletronica","2014"));
+            musica.add(new musica("radioactive ","Rock","2012"));
+            musica.add(new musica("Chop Suey","Rock","2001"));
+            
+}
+         return musica;
+}
+public static void addMusica(musica c){
+     getMusica().add(c);
+}
+public static void setMusica(int index, musica c){
+     getMusica().set(index,c);
+}
+public static void removeMusica(int index){
+     getMusica().remove(index);
+}   
 }
